78cf6241d482de1b8e775bc42980c9759e3d6052
plumbob86 <plumbob86@users.noreply.github.com>
2024-01-21 20:29:32 -0600

    Initial

---
+++ Zoo/src/main/Runner.java
@@ -0,0 +1,17 @@
+package main;
+
+import model.Example;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Runner run = new Runner();
+		run.go();
+	}
+
+	private void go() {
+		Example example = new Example();
+		example.makeNoise();
+		
+	}
+}
